dfd55accfe129ac37700caadbbd98939bea38bdb
dieum512 <akonkwadieum222@gmail.com>
2023-09-19 10:40:12 +0200

    Update the router

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'groups#index'
+
+  resources :groups, only: [:index, :show, :new, :create] do
+    resources :entities, only: [:index]
+  end
+
+  resources :entities, only: [:new, :create]
 end
