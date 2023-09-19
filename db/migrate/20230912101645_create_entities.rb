1609b7a9c356640dd113142d7ea08bdaa75fce52
dieum512 <akonkwadieum222@gmail.com>
2023-09-19 10:41:27 +0200

    Update the entities migation

--- db/migrate/20230912101645_create_entities.rb
+++ db/migrate/20230912101645_create_entities.rb
@@ -1,9 +1,10 @@
 class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
-      t.string :authored
       t.string :name
       t.decimal :amount
+      t.references :user, null: false, foreign_key: true, on_delete: :cascade
+      t.references :group, null: false, foreign_key: true, on_delete: :cascade
 
       t.timestamps
     end
