c97a4929ff6ee1b1c8b08a5dee32adc494b273c7
dieum512 <akonkwadieum222@gmail.com>
2023-09-19 10:42:21 +0200

    Update the groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,36 @@
 class GroupsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
+
+    def index
+        return unless current_user
+
+        @user = current_user
+        @groups = @user.groups.includes(:entities)
+    end
+
+    def show
+        @group = Group.find(params[:id])
+        @entities = @group.entities.order('created_at DESC')
+    end
+
+    def new
+        @group = Group.new
+    end
+
+    def create
+        @group = Group.new(group_params)
+        @group.user_id = current_user.id
+    
+        if @group.save
+          redirect_to groups_path
+        else
+          render :new
+        end
+    end
+    
+    private
+
+    def group_params
+        params.require(:group).permit(:name, :icon)
+    end
 end
