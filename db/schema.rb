85c5485effdccbcf03034bbaae82f2284cacef4d
dieum512 <akonkwadieum222@gmail.com>
2023-09-19 10:42:48 +0200

    Update the schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,18 +15,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_194827) do
   enable_extension "plpgsql"
 
   create_table "entities", force: :cascade do |t|
-    t.string "authored"
     t.string "name"
     t.decimal "amount"
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_entities_on_group_id"
+    t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_194827) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entities", "groups"
+  add_foreign_key "entities", "users"
+  add_foreign_key "groups", "users"
 end
