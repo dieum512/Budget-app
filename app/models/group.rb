94e657d35a40d0d6201fbcf22790fe9a860548e3
dieum512 <akonkwadieum222@gmail.com>
2023-09-23 22:23:53 +0200

    Update the message

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,9 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :entities
 
-  validates :name, :icon, presence: true
+  validates :icon, presence: true
+
+  validates :name, presence: { message: 'Name must not be blank'}
+  validates :icon, presence: { message: 'Name must not be blank'}
   validates :icon, format: { with: %r{\Ahttps?://[^\s]+\z}, message: 'must be a valid URL' }
 end
